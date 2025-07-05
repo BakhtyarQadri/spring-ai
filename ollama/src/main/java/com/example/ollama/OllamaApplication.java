b905c23f32b79d8c46f40cf8874f57c3211b92c3
bakhtyar <muhammad.bakhtyar@intigral.net>
2025-07-05 10:33:14 +0500

    code finalized

--- ollama/src/main/java/com/example/ollama/OllamaApplication.java
+++ ollama/src/main/java/com/example/ollama/OllamaApplication.java
@@ -1,7 +1,7 @@
 package com.example.ollama;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,33 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class OllamaApplication {
 
-    private final OllamaChatModel chatModel;
+    private final ChatModel chatModel;
     private final ChatClient chatClient;
 
-    public OllamaApplication(OllamaChatModel chatModel) {
-        this.chatModel = chatModel;
-        chatClient = ChatClient.create(chatModel);
+    public OllamaApplication(OllamaChatModel ollamaChatModel) {
+        this.chatModel = ollamaChatModel;
+        this.chatClient = ChatClient.create(ollamaChatModel);
     }
 
     public static void main(String[] args) {
         SpringApplication.run(OllamaApplication.class, args);
     }
 
-    @GetMapping("/ollama/chat-model/{question}")
-    public String getAnswerViaChatModel(@PathVariable String question) {
-        try {
-            String response = chatModel.call(question);
-            System.out.println(response);
-            return response;
-        } catch (Exception e) {
-            return e.getMessage();
-        }
+    @GetMapping("/ollama/chat-model/{prompt}")
+    public String getAnswerViaChatModel(@PathVariable String prompt) {
+        String response = chatModel.call(prompt);
+        System.out.println(response);
+        return response;
     }
 
-    @GetMapping("/ollama/chat-client/{question}")
-    public String getAnswerViaChatClient(@PathVariable String question) {
+    @GetMapping("/ollama/chat-client/{prompt}")
+    public String getAnswerViaChatClient(@PathVariable String prompt) {
         try {
-            String response = chatClient.prompt(question).call().content();
+            String response = chatClient.prompt(prompt).call().content();
 //            ChatResponse chatResponse = chatClient.prompt(question).call().chatResponse();
 //            System.out.println(chatResponse.getMetadata().getModel());
 //            response = chatResponse.getResult().getOutput().getText();
